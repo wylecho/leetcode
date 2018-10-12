43987b2ed9178716a5c4d8061cfa837cb5369960
Wenming Wu <wylecho@gmail.com>
2018-10-12 23:28:39 +0800

    prepare comment problem #12

--- algorithms/cpp/src/leetcode_problem_set.cpp
+++ algorithms/cpp/src/leetcode_problem_set.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <map>
 using namespace std;
 
 class Solution
@@ -660,6 +661,67 @@ class Solution
         }
         return maxValue;
     }
+
+    // 12. Integer to Roman
+    /*
+    Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+    Symbol       Value
+    I             1
+    V             5
+    X             10
+    L             50
+    C             100
+    D             500
+    M             1000
+    For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+    
+    Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV.
+    Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+    I can be placed before V (5) and X (10) to make 4 and 9. 
+    X can be placed before L (50) and C (100) to make 40 and 90. 
+    C can be placed before D (500) and M (1000) to make 400 and 900.
+
+    Given an integer, convert it to a roman numeral. Input is guaranteed to be within the range from 1 to 3999.
+    */
+    string intToRoman(int num) {
+        
+    }
+
+    // 13. Roman to Integer
+    /*
+    Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+    */
+    int romanToInt(string s) {
+        map<char, int> charIntMap;
+        charIntMap.insert(make_pair<char, int>('I', 1));
+        charIntMap.insert(make_pair<char, int>('V', 5));
+        charIntMap.insert(make_pair<char, int>('X', 10));
+        charIntMap.insert(make_pair<char, int>('L', 50));
+        charIntMap.insert(make_pair<char, int>('C', 100));
+        charIntMap.insert(make_pair<char, int>('D', 500));
+        charIntMap.insert(make_pair<char, int>('M', 1000));
+
+        int retValue = 0;
+        size_t id = 0;
+        const size_t strLen = s.length();
+        while (id < strLen) {
+            if (id + 1 < strLen) {
+                const int curVal = charIntMap[s[id]];
+                const int nexVal = charIntMap[s[id + 1]];
+                if (curVal < nexVal) {
+                    retValue += (nexVal - curVal);
+                    id += 2;
+                } else {
+                    retValue += curVal;
+                    ++ id;
+                }
+            } else {
+                retValue += charIntMap[s[id]];
+                ++ id;
+            }
+        } 
+        return retValue;
+    }
 };
 
 
@@ -706,8 +768,11 @@ int main()
     //cout << Solution().isMatch("", "c*c*") << endl;
 
     // 11. Container With Most Water
-    vector<int> vec = {1,8,6,2,5,4,8,3,7};
-    cout << Solution().maxArea(vec) << endl;
+    //vector<int> vec = {1,8,6,2,5,4,8,3,7};
+    //cout << Solution().maxArea(vec) << endl;
+
+    // 13. Roman to Integer
+    cout << Solution().romanToInt("MCMXCIV") << endl;
 
     system("pause");
     return 0;
